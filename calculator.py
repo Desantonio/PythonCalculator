e1f06862c908ff176c219bb4c7717225f4c82d67
Vatsal <73216235+Desantonio@users.noreply.github.com>
2020-12-08 15:01:49 +0530

    Add files via upload
    mycommit

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+num1 = float(input("Enter your first number: "))
+op = input("Enter your operator: ")
+num2 = float(input("Enter your second number: "))
+
+if op == "+":
+    print(num1 + num2)
+elif op == "-":
+    print(num1 - num2)
+elif op == "*":
+    print(num1 * num2)
+elif op == "/":
+    print(num1 / num2)
